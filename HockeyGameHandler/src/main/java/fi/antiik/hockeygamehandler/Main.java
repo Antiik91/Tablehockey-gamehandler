26c14910b2bcad84c5ce346112be05e61ea13121
Janne Antikainen <janantik@dhcp-asv-122.helsinki.fi>
2016-05-17 18:01:30 +0300

    Main class created

---
+++ HockeyGameHandler/src/main/java/fi/antiik/hockeygamehandler/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.antiik.hockeygamehandler;
+
+import java.util.*;
+
+/**
+ *
+ * @author janantik
+ *
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hei maailma!");
+    }
+}
