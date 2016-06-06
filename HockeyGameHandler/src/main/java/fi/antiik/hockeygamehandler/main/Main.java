7382b94de80af5a05181b175824e38d837872e5b
Antiik91 <antiik91@gmail.com>
2016-06-06 11:45:42 +0300

    Started working with Swing..

---
+++ HockeyGameHandler/src/main/java/fi/antiik/hockeygamehandler/main/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.antiik.hockeygamehandler.main;
+
+import fi.antiik.hockeygamehandler.gui.GUI;
+import fi.antiik.hockeygamehandler.logic.*;
+import fi.antiik.hockeygamehandler.testuserinterface.TestUI;
+import fi.antiik.hockeygamehandler.logic.Referee;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import java.util.*;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author janantik
+ *
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        GUI gui = new GUI();
+        SwingUtilities.invokeLater(gui);
+//        TestUI ui = new TestUI(scanner);
+//        ui.menu();
+
+
+    }
+}
