1aa6f104a3cde5ae511b02b0d31717b4d0475178
Antiik91 <antiik91@gmail.com>
2016-06-09 11:50:24 +0300

    removed dubplicate buttons and textfields in addPlayer

--- HockeyGameHandler/src/main/java/fi/antiik/hockeygamehandler/gui/GUI.java
+++ HockeyGameHandler/src/main/java/fi/antiik/hockeygamehandler/gui/GUI.java
@@ -207,11 +207,11 @@ public class GUI extends javax.swing.JFrame {
                     BorderFactory.createEmptyBorder(5, 5, 5, 5));
             content.setLayout(new BorderLayout());
 
-            content.add(new JTextField());
+//            content.add(new JTextField());
 
-            JButton playerButton = new JButton("Add Player");
-            playerButton.addActionListener(new AddPlayerListener(this.currentStandingTextField, currentStandings));
-            content.add(playerButton);
+//            JButton playerButton = new JButton("Add Player");
+//            playerButton.addActionListener(new AddPlayerListener(this.currentStandingTextField, currentStandings));
+//            content.add(playerButton);
 
             JTextField newPlayer = new JTextField();
             content.add(newPlayer, BorderLayout.NORTH);
